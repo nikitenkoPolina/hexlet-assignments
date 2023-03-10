e58faaa4ee87ebf220ab0caf9c627ad7d8f79453
Polina <polina.nikitenko@kinoafisha.info>
2023-03-11 01:09:02 +0300

    add new solution option

--- java-collections-ru/streams/src/main/java/exercise/App.java
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -12,5 +12,14 @@ class App {
                         || email.endsWith("@hotmail.com"))
                 .count();
     }
+
+//    public final static List<String> VALID_DOMAINS = List.of("gmail.com", "yandex.ru", "hotmail.com");
+//
+//    public static long getCountOfFreeEmailsPro(List<String> emails) {
+//        return emails.stream()
+//                .map(email -> email.split("@")[1])
+//                .filter(VALID_DOMAINS::contains)
+//                .count();
+//    }
 }
 // END
