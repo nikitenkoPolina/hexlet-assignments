80875f93368356b59ae2a4eb0c24d2d54fb400ae
Polina <polina.nikitenko@kinoafisha.info>
2023-03-17 00:00:38 +0300

    remove empty lines

--- java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -12,32 +12,18 @@ class Segment {
         this.point2 = point2;
     }
 
-    public Point getPoint1() {
-        return point1;
-    }
-
-    public Point getPoint2() {
-        return point2;
-    }
-
     public Point getMidPoint() {
-
         int x = (point1.getX() + point2.getX()) / 2;
         int y = (point1.getY() + point2.getY()) / 2;
-
         return new Point(x, y);
-
     }
 
     public Point getBeginPoint() {
-
         return point1;
     }
 
     public Point getEndPoint() {
         return point2;
     }
-
-
 }
 // END
